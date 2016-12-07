ae5e4547ee6c2f58b1668e4a3913b65465805fd2
Martin Vidner <mvidner@suse.cz>
2016-12-07 18:17:15 +0100

    Preinstall libyui rpms

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,10 @@ $SUDO zypper --non-interactive in --no-recommends osc build \
      graphviz-devel \
      libzypp-devel \
      swig ruby-devel python-devel mono-devel \
-     yast2-devtools yast2-core-devel docbook-xsl-stylesheets
+     yast2-devtools yast2-core-devel docbook-xsl-stylesheets \
+     yast2-ruby-bindings libyui\*[0-9]
+# libyui*.rpm: we will overwrite it with our compiled version
+# but this means RPMs that depend on it will have their deps satisfied
 
 #git clone https://github.com/libyui/libyui-rake
 gem install --user-install libyui-rake
